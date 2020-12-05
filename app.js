6c55a0c4b30d49e35177aa9fbcc50f2117c64fd1
unknown <lobosgonza60@gmail.com>
2020-12-04 23:57:44 -0300

    trabajo carrusel en proceso

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const express = require('express')
+const ejs = require("ejs")
 const app = express()
 const port = process.env.PORT || 8080;
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home")
 })
 
 app.listen(port, () => {
